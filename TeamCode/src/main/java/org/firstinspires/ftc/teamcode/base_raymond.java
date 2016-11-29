874f950107b53471debcf78317ecaa31cf8f4240
ZHANG-PC\ZHANG <bapobap6011@gmail.com>
2016-11-28 17:43:54 -0700

    no message

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/base_raymond.java
@@ -0,0 +1,32 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+/**
+ * Created by Rulon on 2016-11-28.
+ */
+
+
+@TeleOp eleop(name = "K9")
+
+//@disabled
+public class base_raymond extends LinearOpMode
+{
+    @Override
+    public void runOpMode () throws  InterruptedException
+    {
+
+        waitForStart();
+
+        while (opModeIsActive())
+        {
+
+
+
+            idle();
+    }
+
+
+
+}
