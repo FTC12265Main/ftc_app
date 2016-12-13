babc76ea562a0edac6f79035127ac535fff7dd3d
ZHANG-PC\ZHANG <bapobap6011@gmail.com>
2016-12-12 18:13:31 -0700

    Autonomous

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous.java
@@ -0,0 +1,13 @@
+package org.firstinspires.ftc.teamcode;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
+
+/**
+ * Created by ZHANG on 2016-12-12.
+ */
+
+public class Autonomous {
+
+}
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/K9.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/K9.java
@@ -58,52 +58,34 @@ public class K9 extends LinearOpMode
                 motorLeft.setPower(-gamepad1.left_trigger);
                 motorRight.setPower(-gamepad1.right_trigger);
             }
-
             else
             {
                 motorLeft.setPower(gamepad1.left_trigger);
                 motorRight.setPower(gamepad1.right_trigger);
             }
-
-
-
-
-
-
             if (gamepad1.a) // If the A button is pressed
             {
                 Grabber.setPosition(0.8); // Set the grabber to the closed position
                 //System.out.println("The A button was pressed, and the grabber closed.");
             }
-
-
             if (gamepad1.b)
             {
                 Grabber.setPosition(0.0); // Set the grabber to the open position
                 //System.out.println("The B button was pressed, and the grabber opened.");
             }
-
-
-
             if (gamepad1.x)
             {
                 armServo.setPosition(0.88); // Set the arm to the dropped position
                 //System.out.println("The X button was pressed, and the arm dropped.");
             }
-
-
             if (gamepad1.y)
             {
                 armServo.setPosition(0.3); // Set the arm to the lifted position
                 //System.out.println("The Y button was pressed, and the arm lifted.");
             }
-
-
-
             idle();
         }
     }
-
     @Override
    public void stop()
    {
@@ -114,6 +96,4 @@ public class K9 extends LinearOpMode
 
 
    }
-
-
 }
